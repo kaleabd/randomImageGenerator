af5e82b71a47911350c4c18446ece8e86ea4e30b
kaleabd <58647480+kaleabd@users.noreply.github.com>
2022-05-26 19:34:53 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const image = document.querySelector("#image-holders");
 const timer = document.querySelector('#timer')
+const form = document.querySelector('.setSize')
+const generateImage = document.querySelector('#getImage')
 
-const generateRandom = document.querySelector("#getImage");
 var d = new Date();
 var hour = d.getHours();
 var minute = d.getMinutes();
 
 timer.innerHTML = `${hour} : ${minute}`
-image.src = "https://picsum.photos/300/500";
-generateRandom.addEventListener("click", function() {
-    image.src = "https://picsum.photos/500/500";
-    setTimeout(function() {
-        location.reload();
-    }, 10);
-
+form.addEventListener("click", function(event) {
+    event.preventDefault();
+    var imageWidth = document.querySelector('#imageWidth').value
+    var imageHeight = document.querySelector('#imageHeight').value
+    image.width = imageWidth;
+    image.height = imageHeight;
+    image.src = `https://picsum.photos/${imageWidth}/${imageHeight}`;
 
 })
